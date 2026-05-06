23b96e07a73120e5021e9d756f3c0a17aa37311f
Mishail <80946997+mikhailhimei@users.noreply.github.com>
2026-05-06 17:15:14 +0300

    Bind panel events after React render commits

--- frontend/src/dialog-custom-ui-panel.jsx
+++ frontend/src/dialog-custom-ui-panel.jsx
@@ -1,5 +1,4 @@
-﻿import React from 'react';
-import { flushSync } from 'react-dom';
+import React, { useLayoutEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import './dialog-custom-ui-create-scenario.jsx';
 import {
@@ -43,9 +42,13 @@ import { initializePanelState } from './panel/state/init-state.jsx';
 import { PANEL_STYLES } from './panel/styles.jsx';
 import { generateConditionId, newCondition } from './panel/utils.jsx';
 
-const ShadowMarkup = ({ html }) => (
-  <div dangerouslySetInnerHTML={{ __html: html }} />
-);
+const ShadowMarkup = ({ html, onRendered }) => {
+  useLayoutEffect(() => {
+    onRendered?.();
+  }, [html, onRendered]);
+
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
+};
 class DialogCustomUiPanel extends HTMLElement {
   constructor() {
     super();
@@ -87,9 +90,7 @@ class DialogCustomUiPanel extends HTMLElement {
     if (!this._reactRoot) {
       this._reactRoot = createRoot(this.shadowRoot);
     }
-    flushSync(() => {
-      this._reactRoot.render(<ShadowMarkup html={markup} />);
-    });
+    this._reactRoot.render(<ShadowMarkup html={markup} onRendered={() => this._afterReactRender()} />);
   }
 
   _unmountReact() {
@@ -1155,6 +1156,9 @@ class DialogCustomUiPanel extends HTMLElement {
       </div>
     `;
     this._mountReact(markup);
+  }
+
+  _afterReactRender() {
     this._bindEvents();
     this._syncEmbeddedTimerAlarmHass();
   }
